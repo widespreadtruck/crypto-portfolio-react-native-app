ca75ed116de663fa5dff820061f585d2cd9ab217
Nikita Guliaev <gulnikita63@gmail.com>
2023-03-02 18:06:55 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -70,32 +70,6 @@ export interface Chain {
   value: any
 }
 
-// function Section({children, title}: SectionProps): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === "dark"
 
@@ -170,28 +144,11 @@ function App(): JSX.Element {
               <FlatList
                 data={portfolioSummary}
                 renderItem={({ item }) => (
-                  // <Text>
-                  //   {item.symbol}
-                  // </Text>
                   <Text className="text-2xl font-bold text-blue-500">
                     {item.symbol}
                   </Text>
                 )}
               />
-              {/* <Section title="Step One">
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Section>
-              <Section title="See Your Changes">
-                <ReloadInstructions />
-              </Section>
-              <Section title="Debug">
-                <DebugInstructions />
-              </Section>
-              <Section title="Learn More">
-                Read the docs to discover what to do next:
-              </Section> */}
-              {/* <LearnMoreLinks /> */}
             </View>
           </View>
         </SafeAreaView>
@@ -199,24 +156,5 @@ function App(): JSX.Element {
     </>
   )
 }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
 
 export default App
